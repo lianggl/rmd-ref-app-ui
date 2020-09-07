71055437dfb4d8d2a75b21f6863b547f0a3293d2
lianggl <gljh2008@126.com>
2020-09-07 14:09:36 +0800

    Update connect.js

--- tasks/options/connect.js
+++ tasks/options/connect.js
@@ -10,10 +10,10 @@ var config = {
    */
     uaa: {
 
-        clientId: '1',
-        clientSecret: '12345',
-        serverUrl: 'http://xxx-xxx.neuseer.cn',
-        redirect_uri: 'http://xxx.neuseer.cn/callback',
+        clientId: '3',
+        clientSecret: 'vDpwHVndJe6XWezb61GT8XdVjDFLDORAtISBoXtX',
+        serverUrl: 'http://bwttest01.neuseer.cn',
+        redirect_uri: 'http://bwttest01.neuseer.cn/callback',
         defaultClientRoute: '/about'
 
     },
